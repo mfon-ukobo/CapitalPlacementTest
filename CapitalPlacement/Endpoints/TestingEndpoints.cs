a85eec517d48a465b7788bdbd5610f7a1f5681ac
mfon ukobo <coache1234@gmail.com>
2023-05-01 17:58:38 +0100

    added DI

---
+++ CapitalPlacement/Endpoints/TestingEndpoints.cs
@@ -0,0 +1,76 @@
+﻿using Bogus;
+using CapitalPlacement.Database;
+using CapitalPlacement.Enums;
+using CapitalPlacement.Models.ApplicationFormModels;
+using CapitalPlacement.Models.ProgramDetailModels;
+using CapitalPlacement.Services;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapitalPlacement.Endpoints
+{
+    public class TestingEndpoints
+    {
+        private readonly ICosmosContext _context;
+        private readonly IProgramService _programService;
+
+        public TestingEndpoints(ICosmosContext context, IProgramService programService)
+        {
+            _context = context;
+            _programService = programService;
+        }
+
+        public async Task AddProgram()
+        {
+            Console.WriteLine("Adding progrem");
+            var programDetailFaker = new Faker<ProgramDetail>()
+                .RuleFor(x => x.ApplicationClose, val => DateTime.UtcNow)
+                .RuleFor(x => x.ApplicationOpen, val => DateTime.UtcNow)
+                .RuleFor(x => x.ProgramStart, val => DateTime.UtcNow)
+                .RuleFor(x => x.ApplicationCriteria, val => val.Lorem.Paragraph(1))
+                .RuleFor(x => x.Summary, val => val.Lorem.Paragraph(1))
+                .RuleFor(x => x.Description, val => val.Lorem.Paragraph(1))
+                .RuleFor(x => x.Benefits, val => val.Lorem.Paragraph(1))
+                .RuleFor(x => x.Duration, val => "6 months")
+                .RuleFor(x => x.Skills, val => new[] { "Thinking" })
+                .RuleFor(x => x.Title, val => val.Lorem.Text())
+                .RuleFor(x => x.Type, val => ProgramType.Long)
+                .RuleFor(x => x.MinQualification, val => Qualification.High_School)
+                .RuleFor(x => x.MaxApplicationAllowed, val => val.Random.Int(1, 10000));
+
+            var id = Guid.NewGuid().ToString();
+
+            await _programService.CreateAsync(new()
+            {
+                Id = id,
+                Detail = programDetailFaker.Generate(1)[0]
+            });
+
+            Console.WriteLine("Finished Adding progrem");
+        }
+
+        public async Task GetProgram()
+        {
+            var program = await _context.Programs.GetAsync("0a3ad493-a821-4c2d-a381-5ee7c0346134");
+            Console.Write(JsonConvert.SerializeObject(program));
+        }
+
+        public async Task AddApplicationForm()
+        {
+            var applicationForm = new ApplicationForm
+            {
+                CoverImageUrl = "google.com",
+                Id = "0a3ad493-a821-4c2d-a381-5ee7c0346134",
+                ProgramId = "0a3ad493-a821-4c2d-a381-5ee7c0346134",
+                PersonalInfo = new(),
+                Profile = new()
+            };
+
+            await _context.ApplicationForms.AddAsync(applicationForm);
+        }
+    }
+}
--- CapitalPlacement/Program.cs
+++ CapitalPlacement/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CapitalPlacement.Database;
+using CapitalPlacement.Endpoints;
+using CapitalPlacement.Services;
+using CapitalPlacement.Utilities;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var hostBuilder = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        config.AddJsonFile("appsettings.json");
+        config.AddEnvironmentVariables();
+    })
+    .ConfigureServices((hostingContext, services) =>
+    {
+        var configurationSection = hostingContext.Configuration.GetSection("CosmosDb");
+        var databaseName = configurationSection["DatabaseName"];
+        var account = configurationSection["Account"];
+        var key = configurationSection["Key"];
+
+        services.AddSingleton<CosmosDbServiceProvider>(InitializeCosmosClientInstanceAsync(account, key, databaseName).GetAwaiter().GetResult());
+        services.AddSingleton<ICosmosContext, CosmosContext>();
+        services.AddScoped<IProgramService, ProgramService>();
+
+        // TODO: Clean up
+        services.AddScoped<TestingEndpoints>();
+    });
+
+var app = hostBuilder.Build();
+
+var testingService = app.Services.GetRequiredService<TestingEndpoints>();
+await testingService.AddProgram();
+//await testingService.GetProgram();
+//await testingService.AddApplicationForm();
+
+await app.RunAsync();
+
+static async Task<CosmosDbServiceProvider> InitializeCosmosClientInstanceAsync(string account, string key, string databaseName)
+{
+    var client = new CosmosClient(account, key, new()
+    {
+        SerializerOptions = new()
+        {
+            PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
+        }
+    });
+
+    var database = await client.CreateDatabaseIfNotExistsAsync(databaseName);
+    //await database.Database.CreateContainerIfNotExistsAsync(containerName, "/id");
+    return new CosmosDbServiceProvider(account, key, databaseName);
+}
